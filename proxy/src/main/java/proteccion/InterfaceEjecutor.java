864b6d720bfe130844a2e46aed70851b28124eb1
DevBet <frana00@gmail.com>
2024-12-23 15:23:23 -0300

    Agrega patrón Proxy

---
+++ proxy/src/main/java/proteccion/InterfaceEjecutor.java
@@ -0,0 +1,8 @@
+package proteccion;
+
+// Interface "Sujeto"
+public interface InterfaceEjecutor {
+
+    // Método peticion()
+    public String ejecutarComando(String comando) throws Exception;
+}
